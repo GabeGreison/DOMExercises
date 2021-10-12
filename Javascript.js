4ba9c35592fd0a26567808884d4eea44305c0255
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-10-12 20:32:52 -0300

    first dom steps

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+const div = document.createElement('div')
+const body = document.querySelector('body')
+div.innerText = "olá mundo"
+const header = document.querySelector("header")
+const form = body.querySelector('script')
+
+header.insertBefore(div,form)
+ 
